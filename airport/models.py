98a84b83c326971e2115e23ce412a1886666dd38
Nazarii Khalimonov <nazar131188@gmail.com>
2025-07-02 13:45:21 +0300

    create Crew, AirplaneType, Airplane models

--- airport/models.py
+++ airport/models.py
@@ -14,3 +14,32 @@ class Route(models.Model):
     source = models.ForeignKey("Airport", on_delete=models.CASCADE)
     destination = models.ForeignKey("Airport", on_delete=models.CASCADE)
     distance = models.IntegerField()
+
+    def __str__(self):
+        return f"Route: {self.source}, {self.destination}, distance: {self.distance}"
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Crew: {self.first_name}, {self.last_name}"
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Airplane Type: {self.name}"
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey("AirplaneType", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Name: {self.name}, Airplane Type: {self.airplane_type}, rows: {self.rows}, seats_in_row: {self.seats_in_row}"
+
