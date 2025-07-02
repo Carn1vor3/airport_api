0fdcf86e83b9769ea995dbd28882710ea5debe0a
Nazarii Khalimonov <nazar131188@gmail.com>
2025-07-02 13:39:04 +0300

    create Airport and Route models

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length=100)
+    closest_big_city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Airport: {self.name}, closest_big_city: {self.closest_big_city}"
+
+
+class Route(models.Model):
+    source = models.ForeignKey("Airport", on_delete=models.CASCADE)
+    destination = models.ForeignKey("Airport", on_delete=models.CASCADE)
+    distance = models.IntegerField()
