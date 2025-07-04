19d2863d8d8737b5a8d8e38b095a966289ec8e38
Nazarii Khalimonov <nazar131188@gmail.com>
2025-07-04 22:19:24 +0300

    add manage user route

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("me/", ManageUserView.as_view(), name="manage_user"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets, generics
 
-from user.serializers import CreateUserSerializer
+from user.serializers import UserSerializer
 
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
-    serializer_class = CreateUserSerializer
+    serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
